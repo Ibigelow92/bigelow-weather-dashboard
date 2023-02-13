2030767ee630f14432e0d6642d042f4bfead851a
Ibigelow92 <iobbigelow@gmail.com>
2023-02-12 16:07:01 -0800

    did initial HTML

---
+++ script.js
@@ -0,0 +1,12 @@
+//easier to put this in a variable
+var apiKey = 'f0fe7e97b7b37ef60b40ad5cf72fb1b8';
+
+//clears local storage for this session
+localStorage.clear();
+
+//geocode API call
+//http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+//example of geocode API call
+//http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
+
+
